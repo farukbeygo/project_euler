1bcebe3ad372a0e955b9db633249f8bf0c427792
Faruk <109393466+farukbeygo@users.noreply.github.com>
2022-12-03 22:35:43 +0300

    Update problem_3.py

--- problem_3.py
+++ problem_3.py
@@ -6,29 +6,25 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
-import math
+from math import sqrt
 
-number = 600851475143
-num_list = []
-div_list = []
-pri_list = []
+# I will write function that return primeFactors of the given number
+def Factor(x):
+    factor_list = []
+    for factor in range(2, round(sqrt(x))):
+        if x%factor == 0:
+            factor_list.append(factor)
+    return factor_list
 
-
-for num in range(2, round(math.sqrt(number))):
-    num_list.append(num)
-
-for div in num_list:
-    if number%div == 0:
-        div_list.append(div)
-
-
-for pri in div_list:
-    flag = True
-    for pri_div in range(2, round(math.sqrt(pri))):
-        if pri%pri_div == 0:
-            flag = False
-            break
-    if flag:
-        pri_list.append(pri)
-            
-print(max(pri_list))
+# NHere is another function that return primeFactorList of the given number
+def primeFactor(x):
+    primeFactor_list = []
+    for num in Factor(x):
+        flag = True
+        for prime in range(2, round(sqrt(num))):
+            if num%prime == 0:
+                flag = False
+                break
+        if flag:
+            primeFactor_list.append(num)
+    return primeFactor_list
