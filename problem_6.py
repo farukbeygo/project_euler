985654b2016c3397a25bed179ba73cb9d28b788a
Faruk <109393466+farukbeygo@users.noreply.github.com>
2022-12-03 22:37:25 +0300

    Update problem_6.py

--- problem_6.py
+++ problem_6.py
@@ -1,6 +1,7 @@
 """
-Find the difference between the sum of the squares of the first one hundred 
-natural numbers and the square of the sum.
+"The following problem is taken from Project Euler."
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
 
 #that was very basic problem
